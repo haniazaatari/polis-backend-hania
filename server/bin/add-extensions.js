cd26b129d40d17403895ae970017f18c7bc97a78
Bennie Rosas <bennie.rosas@blvd.co>
2024-10-30 01:14:17 -0500

    script to inject .js into local imports

---
+++ server/bin/add-extensions.js
@@ -0,0 +1,17 @@
+import { glob } from 'glob';
+import fs from 'fs/promises';
+
+async function addExtensions() {
+  const files = await glob('src-ts/**/*.ts');
+
+  for (const file of files) {
+    let content = await fs.readFile(file, 'utf8');
+
+    // Add .js to local imports that don't have any extension
+    content = content.replace(/from ['"](\.(?:(?!\.js['"])[^'"])*?)['"];/g, "from '$1.js';");
+
+    await fs.writeFile(file, content);
+  }
+}
+
+addExtensions().catch(console.error);
