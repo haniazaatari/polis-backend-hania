c4a123914855b9d0624911539ec274f64058a1b9
tevko <tim@devzero.io>
2024-11-25 15:49:20 -0600

    begin style dep fixes

--- client-report/src/components/comment.js
+++ client-report/src/components/comment.js
@@ -1,14 +1,12 @@
 // Copyright (C) 2012-present, The Authors. This program is free software: you can redistribute it and/or  modify it under the terms of the GNU Affero General Public License, version 3, as published by the Free Software Foundation. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import React from "react";
-import Radium from "radium";
 import PropTypes from "prop-types";
 import Flex from "./flex";
 // import ParticipantHeader from "./participant-header";
 import * as globals from "./globals";
 import BarChart from "./barChart";
 
-@Radium
 class Comment extends React.Component {
   static propTypes = {
     dispatch: PropTypes.func,
@@ -54,7 +52,8 @@ class Comment extends React.Component {
       {this.props.formatTid(this.props.comment.tid)}
       </span>
       <span style={{fontWeight: 700, fontStyle: "auto"}}>{this.props.comment.is_meta ? "Metadata: " : ''}</span>
-      <span style={[globals.paragraph, {fontStyle: "italic"}]}>{ this.props.comment.txt }</span>
+       {/* style={[globals.paragraph, {fontStyle: "italic"}]} */}
+      <span >{ this.props.comment.txt }</span>
       </Flex>
       <svg width={globals.barChartWidth} height={70}>
       <line
