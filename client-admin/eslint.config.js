4228db30681776371f6b1a3b22239374e0253787
Bennie Rosas <ben@aliencyb.org>
2025-03-17 11:02:36 -0500

    Client-Admin updates and cleanup (#1966)
    * update eslint, babel, and prettier
    
    * more update eslint, babel, and prettier
    
    * WIP new webpack build
    
    * update minor versions
    
    * update dev packages and eslint; remove unused
    
    * remove socials
    
    * improved webpack with hmr
    
    * update Dockerfile
    
    * update nodes; enable custom apiUrl in dev
    
    * bump client-admin workflows to node 20
    
    * install markdown-loader
    
    * remove bundlewatch
    
    * favicon; mdx-loader

---
+++ client-admin/eslint.config.js
@@ -0,0 +1,92 @@
+const eslint = require('@eslint/js');
+const globals = require('globals');
+const jsxA11yPlugin = require('eslint-plugin-jsx-a11y');
+const reactPlugin = require('eslint-plugin-react');
+const importPlugin = require('eslint-plugin-import');
+const babelParser = require('@babel/eslint-parser');
+
+module.exports = [
+  {
+    // Base configuration for all files
+    ignores: ['build/**'],
+  },
+  eslint.configs.recommended,
+  {
+    // Import plugin recommended rules
+    files: ['**/*.js', '**/*.jsx'],
+    name: 'import-recommended',
+    rules: {
+      ...importPlugin.configs.recommended.rules,
+    }
+  },
+  {
+    // JSX-a11y plugin recommended rules
+    files: ['**/*.js', '**/*.jsx'],
+    name: 'jsx-a11y-recommended',
+    rules: {
+      ...jsxA11yPlugin.configs.recommended.rules,
+    }
+  },
+  {
+    // React plugin recommended rules
+    files: ['**/*.js', '**/*.jsx'],
+    name: 'react-recommended',
+    rules: {
+      ...reactPlugin.configs.recommended.rules,
+    }
+  },
+  {
+    // Main configuration with your custom rules (should come AFTER the plugin configs)
+    files: ['**/*.js', '**/*.jsx'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      parser: babelParser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        },
+        requireConfigFile: false,
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+        process: 'readonly',
+        // jQuery globals
+        $: 'readonly',
+        jQuery: 'readonly',
+      },
+    },
+    plugins: {
+      'jsx-a11y': jsxA11yPlugin,
+      'react': reactPlugin,
+      'import': importPlugin,
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      },
+      'import/resolver': {
+        node: { extensions: ['.js', '.jsx'] }
+      }
+    },
+    rules: {
+      camelcase: 'off',
+      'import/namespace': 'warn',
+      'jsx-a11y/no-static-element-interactions': 'warn',
+      'jsx-a11y/tabindex-no-positive': 'warn',
+      'object-shorthand': 'off',
+      'no-unused-vars': ['error', { args: 'none' }],
+      'react/no-unknown-property': ['error', { ignore: ['sx'] }]
+    }
+  },
+  {
+    // Override for Node.js files
+    files: ['webpack.config.js', 'eslint.config.js'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  }
+]; 
--- client-admin/webpack.config.js
+++ client-admin/webpack.config.js
@@ -1,155 +1,141 @@
-// Copyright (C) 2012-present, The Authors. This program is free software: you can redistribute it and/or  modify it under the terms of the GNU Affero General Public License, version 3, as published by the Free Software Foundation. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details. You should have received a copy of the GNU Affero General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import path from 'path'
+import HtmlWebPackPlugin from 'html-webpack-plugin'
+import LodashModuleReplacementPlugin from 'lodash-webpack-plugin'
+import CompressionPlugin from 'compression-webpack-plugin'
+import CopyPlugin from 'copy-webpack-plugin'
+import TerserPlugin from 'terser-webpack-plugin'
+import EventHooksPlugin from 'event-hooks-webpack-plugin'
+import * as glob from 'glob'
+import fs from 'fs'
+import { fileURLToPath } from 'url'
 
-var path = require("path");
-var webpack = require("webpack");
-var CompressionPlugin = require('compression-webpack-plugin');
-var LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
-var HtmlWebPackPlugin = require('html-webpack-plugin');
-var EventHooksPlugin = require('event-hooks-webpack-plugin');
-var CopyPlugin = require("copy-webpack-plugin");
-var TerserPlugin = require("terser-webpack-plugin");
-var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
-var mri = require('mri');
-var glob = require('glob');
-var fs = require('fs');
+// Get __dirname equivalent in ESM
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-// CLI commands for deploying built artefact.
-var argv = process.argv.slice(2)
-var cliArgs = mri(argv)
+export default (env, argv) => {
+  const isProduction = argv.mode === 'production'
+  const isDevelopment = !isProduction
 
-var enableTwitterWidgets = process.env.ENABLE_TWITTER_WIDGETS === 'true';
-var fbAppId = process.env.FB_APP_ID;
+  // Get API URL from CLI arg, env var, or default
+  const apiUrl = env?.apiUrl || process.env.API_URL || 'http://localhost:5000'
 
-module.exports = (env, options) => {
-  var isDevBuild = options.mode === 'development';
-  var isDevServer = process.env.WEBPACK_SERVE;
-  var chunkHashFragment = (isDevBuild || isDevServer) ? '' : '.[chunkhash:8]';
+  console.log(`Using API URL: ${apiUrl}`)
+
   return {
-    entry: ["./src/index"],
+    mode: isProduction ? 'production' : 'development',
+    entry: './src/index.js',
     output: {
+      path: path.join(__dirname, 'build'),
+      filename: isProduction
+        ? 'static/js/admin_bundle.[contenthash].js'
+        : 'static/js/admin_bundle.js',
       publicPath: '/',
-      filename: `static/js/admin_bundle${chunkHashFragment}.js`,
-      path: path.resolve(__dirname, "build"),
-      clean: true,
+      clean: true
     },
-    resolve: {
-      extensions: [".js", ".css", ".png", ".svg"],
-    },
-    devServer: {
+    devtool: isDevelopment ? 'eval-source-map' : 'source-map',
+    devServer: isDevelopment ? {
       historyApiFallback: true,
-      /**
-      // TODO: Set up API proxy later for server component.
-      // See: https://webpack.js.org/configuration/dev-server/#devserverproxy
-      proxy: {
-        '/api': {
-        target: 'https://pol.is',
-        secure: false,
-        },
-      },
-      **/
-    },
-    plugins: [
-      new CopyPlugin({
-        patterns: [
-          { from: 'public', globOptions: { ignore: ['**/index.ejs']}},
-        ],
-      }),
-      new HtmlWebPackPlugin({
-        template: path.resolve( __dirname, 'public/index.ejs' ),
-        filename: (isDevBuild || isDevServer) ? 'index.html' : 'index_admin.html',
-        inject: "body",
-        templateParameters: {
-          enableTwitterWidgets: enableTwitterWidgets,
-          fbAppId: fbAppId,
-        },
-      }),
-      new LodashModuleReplacementPlugin({
-        currying: true,
-        flattening: true,
-        paths: true,
-        placeholders: true,
-        shorthands: true
-      }),
-      new webpack.DefinePlugin({
-        'process.env.FB_APP_ID': JSON.stringify(fbAppId),
-      }),
-      // Only run analyzer when specified in flag.
-      ...(cliArgs.analyze ? [new BundleAnalyzerPlugin({ defaultSizes: 'gzip' })] : []),
-      // Only compress and create headerJson files during production builds.
-      ...((isDevBuild || isDevServer || cliArgs.analyze) ? [] : [
-        new CompressionPlugin({
-          test: /\.js$/,
-          // Leave unmodified without gz ext.
-          // See: https://webpack.js.org/plugins/compression-webpack-plugin/#options
-          filename: '[path][base]',
-          deleteOriginalAssets: true,
-        }),
-        new EventHooksPlugin({
-          afterEmit: () => {
-            console.log('Writing *.headersJson files...')
-
-            function writeHeadersJson(matchGlob, headersData = {}) {
-              const files = glob.sync(path.resolve(__dirname, "build", matchGlob))
-              files.forEach((f, i) => {
-                const headersFilePath = f + '.headersJson'
-                fs.writeFileSync(headersFilePath, JSON.stringify(headersData))
-              })
-            }
-
-            function writeHeadersJsonHtml() {
-              const headersData = {
-                'Content-Type': 'text/html; charset=UTF-8',
-                'Cache-Control': 'no-cache'
-              }
-              writeHeadersJson('*.html', headersData)
-            }
-
-            function writeHeadersJsonJs() {
-              const headersData = {
-                'Content-Encoding': 'gzip',
-                'Content-Type': 'application/javascript',
-                'Cache-Control':
-                  'no-transform,public,max-age=31536000,s-maxage=31536000'
-              }
-              writeHeadersJson('static/js/*.js?(.map)', headersData)
-            }
-
-            function writeHeadersJsonMisc() {
-              writeHeadersJson('favicon.ico')
-            }
-
-            writeHeadersJsonHtml()
-            writeHeadersJsonJs()
-            writeHeadersJsonMisc()
-          }
-        }),
-      ])
-    ],
-    optimization: {
-      minimize: !isDevBuild,
-      minimizer: [new TerserPlugin()],
-    },
+      hot: true,
+      port: 3000,
+      proxy: [
+        {
+          context: ['/api'],
+          target: apiUrl,
+          secure: false,
+        }
+      ],
+    } : {},
     module: {
       rules: [
         {
-          test: /\.m?js$/,
+          test: /\.js$/,
           exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env', '@babel/preset-react'],
-            },
-          },
+          use: ['babel-loader']
         },
         {
-          test: /\.(png|jpg|gif|svg)$/,
-          loader: 'file-loader',
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
         },
         {
           test: /\.mdx?$/,
           use: ['babel-loader', '@mdx-js/loader']
         }
+      ]
+    },
+    optimization: {
+      minimize: isProduction,
+      minimizer: [
+        new TerserPlugin(),
       ],
     },
-  };
+    plugins: [
+      // Common plugins for both production and development
+      new HtmlWebPackPlugin({
+        template: 'public/index.html',
+        filename: isProduction ? 'index_admin.html' : 'index.html',
+        inject: 'body',
+      }),
+
+      // Production-only plugins
+      isProduction && new LodashModuleReplacementPlugin({
+        collections: true,
+        shorthands: true
+      }),
+
+      isProduction && new CopyPlugin({
+        patterns: [
+          {
+            from: 'public',
+            to: '',
+            globOptions: {
+              ignore: ['**/index.html']
+            }
+          },
+          {
+            from: 'public/favicon.ico',
+            to: ''
+          }
+        ],
+      }),
+
+      isProduction && new CompressionPlugin({
+        test: /\.js$/,
+        filename: '[path][base]',
+        algorithm: 'gzip',
+        deleteOriginalAssets: true,
+      }),
+
+      isProduction && new EventHooksPlugin({
+        afterEmit: () => {
+          console.log('Writing *.headersJson files...')
+
+          function writeHeadersJson(matchGlob, headersData = {}) {
+            const files = glob.sync(path.resolve(__dirname, "build", matchGlob))
+            files.forEach((f) => {
+              const headersFilePath = f + '.headersJson'
+              fs.writeFileSync(headersFilePath, JSON.stringify(headersData))
+            })
+          }
+
+          // Headers for HTML files
+          writeHeadersJson('*.html', {
+            'Content-Type': 'text/html; charset=UTF-8',
+            'Cache-Control': 'no-cache'
+          })
+
+          // Headers for JS files
+          writeHeadersJson('static/js/*.js', {
+            'Content-Encoding': 'gzip',
+            'Content-Type': 'application/javascript',
+            'Cache-Control': 'no-transform,public,max-age=31536000,s-maxage=31536000'
+          })
+
+          // Headers for other files
+          writeHeadersJson('favicon.ico')
+        }
+      }),
+    ].filter(Boolean)
+  }
 }
+
