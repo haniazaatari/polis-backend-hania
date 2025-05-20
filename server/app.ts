04ebdfbd0dd21ee1d43c488a9d50b6cec85d2367
Tim <timevko@gmail.com>
2025-05-20 12:09:59 -0500

    make arch ready (#2014)
    * make arch ready
    
    * begin splitting up cdk code
    
    * organize cdk
    
    * cdk efs and permissions fixes
    
    * docker fixes, add block storage
    
    * remove default dynamo endpoint
    
    * endpoint check fix
    
    * use us-east-1
    
    * remove more hardcoded values
    
    * adjust delphi dockerfile
    
    * fix up setupdynamo func
    
    * fix reference
    
    * add ssl mode hardcoded
    
    * another place to add secure mode
    
    * ssl fix
    
    * update iam roles
    
    * remove hardcoded minio from docker
    
    * remove s3 check
    
    * change s3 endpoint to none if missingf
    
    * more s3 config
    
    * fix indentation
    
    * s3 logic fix
    
    * indentation fix
    
    * remove env check
    
    * comment out s3 creds
    
    * remove location constraint
    
    * unique bucket name
    
    * remove public read on aws
    
    * fix comment
    
    * endpoint urls in s3
    
    * remove another dynamo default
    
    * more endpoint config
    
    * lint and test cleanup
    
    * more lint fix
    
    * defaults fixes
    
    * tsc fix
    
    * attempt free up disk space
    
    * multi stage builds
    
    * more space optimization
    
    * more optimization
    
    * debug
    
    * remove commented out docker code, readme and makefile updates

--- server/app.ts
+++ server/app.ts
@@ -791,7 +791,6 @@ helpersInitialized.then(
       try {
         handle_POST_delphi_jobs(req, res);
       } catch (err) {
-        console.error("Error in delphi jobs creation route:", err);
         res.json({
           status: "error",
           message: "Internal server error in job creation endpoint",
@@ -805,7 +804,6 @@ helpersInitialized.then(
       try {
         handle_GET_delphi_reports(req, res);
       } catch (err) {
-        console.error("Error in delphi reports route:", err);
         res.json({
           status: "error",
           message: "Internal server error in reports endpoint",
@@ -821,7 +819,6 @@ helpersInitialized.then(
       try {
         handle_GET_delphi_visualizations(req, res);
       } catch (err) {
-        console.error("Error in delphi visualizations route:", err);
         res.json({
           status: "error",
           message: "Internal server error in visualizations endpoint",
@@ -835,7 +832,6 @@ helpersInitialized.then(
       try {
         handle_POST_delphi_batch_reports(req, res);
       } catch (err) {
-        console.error("Error in delphi batch reports route:", err);
         res.json({
           status: "error",
           message: "Internal server error in batch reports endpoint",
