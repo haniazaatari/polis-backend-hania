c8913a35c7defef8559a9aad2b6781bfa4bd035d
tevko <tim@devzero.io>
2025-09-29 20:34:01 -0500

    fix handler call

--- cdk/lambda/lambda.ts
+++ cdk/lambda/lambda.ts
@@ -9,7 +9,7 @@ import * as ec2 from 'aws-cdk-lib/aws-ec2';
 const createDBBackupLambda = (self: Construct, db: cdk.aws_rds.DatabaseInstance, vpc: cdk.aws_ec2.IVpc, dbBackupBucket: cdk.aws_s3.Bucket, dbBackupLambdaRole: iam.Role) => {
   return new lambda.Function(self, 'DBBackupLambda', {
     runtime: lambda.Runtime.PYTHON_3_9,
-    handler: 'handler.lambda_handler',
+    handler: 'dbBackuplambda.lambda_handler',
     code: lambda.Code.fromAsset('lambda/handler'),
     vpc: vpc,
     vpcSubnets: { subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS },
